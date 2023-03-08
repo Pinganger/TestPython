fe08fe5396daf7d8a3fb4769cec62271f764d68c
Pinganger <yaopinghua673@outlook.com>
2023-03-08 16:19:19 +0800

    Test Python

---
+++ HelloPinganger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName ：HelloPinganger.py
+# @Time     ：2023/3/8 15:53
+# @Author   : Pinganger
+
+print ('Hello Pinganger')
+
+# if __name__ == "__main__":
