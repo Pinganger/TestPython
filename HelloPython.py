ac43cdc42b563f2079fd50714f52a1b8c4b788cc
pinganger <yaopinghua673@outlook.com>
2023-03-19 15:56:02 +0800

    Test Python

--- HelloPython.py
+++ HelloPython.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @FileName ：HelloPython.py
-# @Time     ：2023/3/8 15:53
+# ========================================
+# @FileName ：123123.py
+# @Time     ：2023/3/19 15:53
 # @Author   : Pinganger
-
+# @Email    ：yaopinghua673@outlook.com
+# @Company  ：Hua'an CO.,LTD.
+# ========================================
 print('Hello Pinganger')
--- passWords.py
+++ passWords.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @FileName ：passWords.py
-# @Time     ：2023/3/8 15:55
+# ========================================
+# @FileName ：123123.py
+# @Time     ：2023/3/19 15:53
 # @Author   : Pinganger
-
+# @Email    ：yaopinghua673@outlook.com
+# @Company  ：Hua'an CO.,LTD.
+# ========================================
 
 import random
 import os
