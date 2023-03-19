860ff0eb177cc78908710a26704d33763891c5dc
pinganger <yaopinghua673@outlook.com>
2023-03-19 16:58:32 +0800

    dataTpye

---
+++ Day1/dataType.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# ========================================
+# @FileName ：dataType.py
+# @Time     ：2023/3/19 16:30
+# @Author   : Pinganger
+# @Email    ：yaopinghua673@outlook.com
+# @Company  ：Hua'an CO.,LTD.
+# ========================================
+# 数据类型
+# 1）数值类型：整数、小数（浮点数）
+"""
+数值类型的运算:
+1、算术运算符:
+加 + 减- 乘* 除/ 除法 取整 //除法取余%幂运算 **比较运算符:I
+3、比较运算符:大于:>小于:<大于等于: >=小于等于:<=不等于: !=等等:==
+"""
+age = 28
+print(5**3)
+print(10*2 == 100)
+
+print("test Python'class")
+print('test python"class')
+print('test python\"class')
+
+aa = 'p\tython"class'
+bb = 'py\tthon"class'
+cc = 'pyt\thon"class'
+dd = 'pyth\ton"class'
+print(aa)
+print(bb)
+print(cc)
+print(dd)
+# if __name__ == "__main__":
