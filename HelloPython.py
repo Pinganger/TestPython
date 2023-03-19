2fedc78ffa8fac7f3686f7679b004b2e417b88a5
pinganger <yaopinghua673@outlook.com>
2023-03-19 16:11:54 +0800

    Test Python

--- HelloPython.py
+++ HelloPython.py
@@ -7,4 +7,13 @@
 # @Email    ：yaopinghua673@outlook.com
 # @Company  ：Hua'an CO.,LTD.
 # ========================================
+import keyword
+
 print('Hello Pinganger')
+
+# 打印Pyhton内置35个关键字：
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
+# 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for',
+# 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or',
+# 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print(keyword.kwlist)
